0e9b18f8308930752b200b52432c4474d42a4be9
2151568 <2151568@LTIN433448.cts.com>
2024-01-03 16:05:36 +0530

    2nd time

--- Ecommerce-website/src/main/java/com/spring/ecommerce/test/Test.java
+++ Ecommerce-website/src/main/java/com/spring/ecommerce/test/Test.java
@@ -2,7 +2,7 @@ package com.spring.ecommerce.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("only test");
+		System.out.println("only test by papai shee");
 	}
 
 }
